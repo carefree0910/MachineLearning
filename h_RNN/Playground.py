62ae0134cdc94d04ec959e8b3ffdf62382451dd3
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2017-05-12 11:44:16 +0800

    Update RNN Example

--- h_RNN/Playground.py
+++ h_RNN/Playground.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.layers as layers
+import matplotlib.pyplot as plt
 
 from g_CNN.Optimizers import OptFactory
 from Util.ProgressBar import ProgressBar
@@ -93,28 +94,24 @@ class LSTM:
         self._sess = tf.Session()
 
         self._params = {
-            "generator_params": kwargs.get("generator_params", {}),
             "n_time_step": kwargs.get("n_time_step", 6),
-            "random_scale": kwargs.get("random_scale", 2),
+            "random_scale": kwargs.get("random_scale", 1),
             "n_hidden": kwargs.get("n_hidden", 64),
             "activation": kwargs.get("activation", tf.nn.sigmoid),
             "lr": kwargs.get("lr", 0.01),
-            "epoch": kwargs.get("epoch", 20),
+            "epoch": kwargs.get("epoch", 25),
             "n_iter": kwargs.get("n_iter", 128),
             "optimizer": kwargs.get("optimizer", "Adam"),
             "batch_size": kwargs.get("batch_size", 64),
             "eps": kwargs.get("eps", 1e-8),
-            "verbose": kwargs.get("verbose", True)
+            "verbose": kwargs.get("verbose", 0)
         }
 
     def __getitem__(self, item):
-        return getattr(self, str(item), None)
+        return getattr(self, "_{}".format(item), None)
 
-    def fit(self, im, om, generator, generator_params=None,
-            n_time_step=None, random_scale=None, n_hidden=None, activation=None,
+    def fit(self, im, om, generator, n_time_step=None, random_scale=None, n_hidden=None, activation=None,
             lr=None, epoch=None, n_iter=None, batch_size=None, optimizer=None, eps=None, verbose=None):
-        if generator_params is None:
-            generator_params = self._params["generator_params"]
         if n_time_step is None:
             n_time_step = self._params["n_time_step"]
         if random_scale is None:
@@ -138,17 +135,17 @@ class LSTM:
         if verbose is None:
             verbose = self._params["verbose"]
 
-        self._generator = generator(n_time_step, random_scale, im, **generator_params)
+        self._generator = generator(n_time_step, random_scale, im, om)
         self._optimizer = OptFactory().get_optimizer_by_name(optimizer, lr)
         self._tfx = tf.placeholder(tf.float32, shape=[None, None, im])
         self._tfy = tf.placeholder(tf.float32, shape=[None, None, om])
 
         cell = LSTMCell(n_hidden)
-        initial_state = cell.zero_state(batch_size, tf.float32)
+        initial_state = cell.zero_state(tf.shape(self._tfx)[1], tf.float32)
         rnn_outputs, rnn_states = tf.nn.dynamic_rnn(
             cell, self._tfx, initial_state=initial_state, time_major=True)
         self._output = tf.map_fn(
-            lambda _x: layers.fully_connected(_x, num_outputs=om, activation_fn=activation),
+            lambda x: layers.fully_connected(x, num_outputs=om, activation_fn=activation),
             rnn_outputs
         )
         err = -tf.reduce_mean(
@@ -158,46 +155,51 @@ class LSTM:
         self._log["iter_err"] = []
         self._log["epoch_err"] = []
 
-        with self._sess.as_default() as sess:
-            sess.run(tf.global_variables_initializer())
-            bar = ProgressBar(max_value=epoch, name="Epoch")
-            for _ in range(epoch):
-                epoch_err = 0
-                sub_bar = ProgressBar(max_value=n_iter, name="Iter")
-                for __ in range(n_iter):
-                    x, y = self._generator.gen(batch_size)
-                    iter_err = sess.run([err, train_step], {
-                        self._tfx: x, self._tfy: y,
-                    })[0]
-                    self._log["iter_err"].append(iter_err)
-                    epoch_err += iter_err
+        self._sess.run(tf.global_variables_initializer())
+        bar = ProgressBar(max_value=epoch, name="Epoch", start=False)
+        if verbose >= 2:
+            bar.start()
+        for _ in range(epoch):
+            epoch_err = 0
+            sub_bar = ProgressBar(max_value=n_iter, name="Iter", start=False)
+            if verbose >= 2:
+                sub_bar.start()
+            for __ in range(n_iter):
+                x_batch, y_batch = self._generator.gen(batch_size)
+                iter_err = self._sess.run([err, train_step], {
+                    self._tfx: x_batch, self._tfy: y_batch,
+                })[0]
+                self._log["iter_err"].append(iter_err)
+                epoch_err += iter_err
+                if verbose >= 2:
                     sub_bar.update()
-                self._log["epoch_err"].append(epoch_err / n_iter)
-                if verbose:
-                    self._verbose(sess)
-                bar.update()
+            self._log["epoch_err"].append(epoch_err / n_iter)
+            if verbose >= 1:
+                self._verbose()
+                if verbose >= 2:
+                    bar.update()
 
-    def _verbose(self, sess):
+    def _verbose(self):
         pass
 
 
 class LSTMForAddition(LSTM):
-    def _verbose(self, sess):
+    def _verbose(self):
         x_test, y_test = self._generator.gen(1)
-        ans = np.argmax(sess.run(self._output, {
+        ans = np.argmax(self._sess.run(self._output, {
             self._tfx: x_test
         }), axis=2).ravel()
         x_test = x_test.astype(np.int)
         print("I think {} = {}, answer: {}...".format(
             " + ".join(
-                ["".join(map(lambda n: str(n), x_test[..., 0, i])) for i in range(x_test.shape[2])]
+                ["".join(map(lambda n: str(n), x_test[..., 0, i][::-1])) for i in range(x_test.shape[2])]
             ),
-            "".join(map(lambda n: str(n), ans)),
-            "".join(map(lambda n: str(n), np.argmax(y_test, axis=2).ravel()))))
+            "".join(map(lambda n: str(n), ans[::-1])),
+            "".join(map(lambda n: str(n), np.argmax(y_test, axis=2).ravel()[::-1]))))
 
 
-class AddGenerator:
-    def __init__(self, n_time_step, random_scale, im, base=10):
+class AdditionGenerator:
+    def __init__(self, n_time_step, random_scale, im, base):
         self._n_time_step = n_time_step
         self._random_scale = random_scale
         self._im, self._base = im, base
@@ -212,9 +214,9 @@ class AddGenerator:
     def gen(self, batch_size):
         n_time_step = self._n_time_step + random.randint(0, self._random_scale)
         x = np.empty([n_time_step, batch_size, self._im])
-        y = np.zeros([n_time_step, batch_size, self._im])
+        y = np.zeros([n_time_step, batch_size, self._base])
         for i in range(batch_size):
-            targets = [int(random.randint(0, self._base ** self._n_time_step - 1) / self._im) for _ in range(self._im)]
+            targets = [int(random.randint(0, self._base ** n_time_step - 1) / self._im) for _ in range(self._im)]
             sequences = [self._gen_seq(n_time_step, tar) for tar in targets]
             for j in range(self._im):
                 x[:, i, j] = sequences[j]
@@ -225,6 +227,14 @@ if __name__ == '__main__':
     # n_sample = 5
     # rnn = RNN1(np.eye(n_sample), np.eye(n_sample), np.eye(n_sample) * 2)
     # print(rnn.run(np.eye(n_sample)))
-    length, _base = 2, 2
-    lstm = LSTMForAddition(generator_params={"base": _base}, n_time_step=2)
-    lstm.fit(length, _base, generator=AddGenerator)
+    _digit_len, _digit_base, _n_digit = 4, 10, 3
+    lstm = LSTMForAddition(n_time_step=_digit_len, epoch=100, n_hidden=128, random_scale=2, verbose=1)
+    lstm.fit(_n_digit, _digit_base, generator=AdditionGenerator)
+    ee, ie = lstm["log"]["epoch_err"], lstm["log"]["iter_err"]
+    ee_base = np.arange(len(ee))
+    ie_base = np.linspace(0, len(ee)-1, len(ie))
+    plt.figure()
+    plt.plot(ie_base, ie, label="Iter error")
+    plt.plot(ee_base, ee, linewidth=3, label="Epoch error")
+    plt.legend()
+    plt.show()
