6f44bfb581271e687c2b212d5198d3b20b6a4f53
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2017-05-14 00:16:00 +0800

    Implemented SparseRNN

---
+++ h_RNN/SpRNN.py
@@ -0,0 +1,118 @@
+import random
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.layers as layers
+
+from h_RNN.RNN import RNNWrapper, OpGenerator
+
+
+class SparseRNN(RNNWrapper):
+    def __init__(self, **kwargs):
+        super(SparseRNN, self).__init__(**kwargs)
+        self._params["n_history"] = kwargs.get("n_history", 1)
+        self._params["activation"] = kwargs.get("activation", None)
+
+    def _define_input(self, im, om):
+        self._tfx = tf.placeholder(tf.float32, shape=[None, None, im])
+        self._tfy = tf.placeholder(tf.int32, shape=[None])
+
+    def _get_loss(self, eps):
+        return tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self._tfy, logits=self._output)
+        )
+
+    def _get_output(self, rnn_outputs, rnn_states, n_history):
+        if n_history == 0:
+            raise ValueError("SparseRNN could not be as dynamic as RNN, so 'n_history' should be positive integer")
+        if n_history == 1:
+            output = rnn_outputs[..., -1, :]
+        else:
+            output = rnn_outputs[..., -n_history:, :]
+            output = tf.reshape(output, [-1, n_history * int(output.get_shape()[2])])
+        self._output = layers.fully_connected(
+            output, num_outputs=self._om, activation_fn=self._activation)
+
+
+class SpRNNForOp(SparseRNN):
+    def __init__(self, **kwargs):
+        super(SpRNNForOp, self).__init__(**kwargs)
+        if not self._params["generator_params"]:
+            self._params["generator_params"] = {
+                "n_time_step": 6, "random_scale": 2
+            }
+        self._params["boost"] = 0
+        self._op = ""
+
+    def _verbose(self):
+        x_test, y_test = self._generator.gen(1, boost=self._params["boost"])
+        ans = np.argmax(self._sess.run(self._output, {self._tfx: x_test}), axis=1)
+        x_test = x_test.astype(np.int)
+        print("I think {} = {}, answer: {}...".format(
+            " {} ".format(self._op).join(
+                ["".join(map(lambda n: str(n), x_test[0, ..., i][::-1])) for i in range(x_test.shape[2])]
+            ),
+            "".join(map(lambda n: str(n), ans)),
+            "".join(map(lambda n: str(n), y_test))))
+
+
+class SpRNNForAddition(SpRNNForOp):
+    def __init__(self, **kwargs):
+        super(SpRNNForAddition, self).__init__(**kwargs)
+        self._op = "+"
+
+
+class SpRNNForMultiple(SpRNNForOp):
+    def __init__(self, **kwargs):
+        super(SpRNNForMultiple, self).__init__(**kwargs)
+        self._op = "*"
+
+
+class SpOpGenerator(OpGenerator):
+    def __init__(self, im, base, n_time_step, random_scale):
+        super(SpOpGenerator, self).__init__(im, base, n_time_step, random_scale)
+        self._base = round(self._base ** (1 / (n_time_step + random_scale)))
+
+    def gen(self, batch_size, test=False, boost=0):
+        if boost:
+            n_time_step = self._n_time_step + self._random_scale + random.randint(1, boost)
+        else:
+            n_time_step = self._n_time_step + random.randint(0, self._random_scale)
+        x = np.empty([batch_size, n_time_step, self._im])
+        y = np.zeros(batch_size, dtype=np.int32)
+        for i in range(batch_size):
+            targets = self._gen_targets(n_time_step)
+            sequences = [self._gen_seq(n_time_step, tar) for tar in targets]
+            for j in range(self._im):
+                x[i, ..., j] = sequences[j]
+            y[i] = self._op(targets)
+        return x, y
+
+
+class SpAdditionGenerator(SpOpGenerator):
+    def _op(self, seq):
+        return sum(seq)
+
+    def _gen_targets(self, n_time_step):
+        return [int(random.randint(0, self._base ** n_time_step - 1) / self._im) for _ in range(self._im)]
+
+
+class SpMultipleGenerator(SpOpGenerator):
+    def _op(self, seq):
+        return np.prod(seq)
+
+    def _gen_targets(self, n_time_step):
+        return [int(random.randint(0, self._base ** n_time_step - 1) ** (1 / self._im)) for _ in range(self._im)]
+
+if __name__ == '__main__':
+    _random_scale = 0
+    _digit_len, _digit_base, _n_digit = 4, 10, 2
+    lstm = SpRNNForMultiple(
+        # cell=tf.contrib.rnn.GRUCell,
+        # cell=tf.contrib.rnn.LSTMCell,
+        # cell=tf.contrib.rnn.BasicRNNCell,
+        # cell=tf.contrib.rnn.BasicLSTMCell,
+        epoch=20, n_history=3,
+        generator_params={"n_time_step": _digit_len, "random_scale": _random_scale}
+    )
+    lstm.fit(_n_digit, _digit_base ** (_digit_len + _random_scale), SpMultipleGenerator)
+    lstm.draw_err_logs()
