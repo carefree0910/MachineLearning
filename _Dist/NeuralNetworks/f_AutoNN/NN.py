8b95b5a8530fcd13f1ed0a5bd7e75c8063ff1649
carefree0910 <syameimaru.saki@gmail.com>
2017-11-18 13:24:13 +0800

    Upload _Dist/NeuralNetworks/f_AutoNN

--- _Dist/NeuralNetworks/f_AutoNN/NN.py
+++ _Dist/NeuralNetworks/f_AutoNN/NN.py
@@ -14,9 +14,8 @@ from _Dist.NeuralNetworks.e_AdvancedNN.NN import Advanced
 
 
 class Auto(Advanced):
-    def __init__(self, x=None, y=None, x_cv=None, y_cv=None, name=None, loss=None, metric=None,
-                 n_epoch=32, max_epoch=256, n_iter=128, batch_size=128, optimizer="Adam", lr=1e-3,
-                 numerical_idx=None, feature_sets=None, is_regression=None,
+    def __init__(self, x=None, y=None, x_test=None, y_test=None, name=None, loss=None, metric=None,
+                 n_epoch=32, max_epoch=256, n_iter=-1, batch_size=128, optimizer="Adam", lr=1e-3,
                  pre_process_settings=None, nan_handler_settings=None, **kwargs):
         if name is None:
             raise ValueError("name should be provided in AutoNN")
@@ -42,47 +41,56 @@ class Auto(Advanced):
         self._nan_handler = None
         self.nan_handler_method = self.reuse_nan_handler_values = None
 
-        self._data_loaded = False
-        self._whether_redundant = None
-        self._numerical_idx = numerical_idx
-        self._is_regression = is_regression
-        self._feature_sets = feature_sets
-        self._is_numeric_label = None
-        self._sparsity = kwargs.pop("sparsity", None)
-        self._class_prior = kwargs.pop("class_prior", None)
-        self._n_features = self._categorical_columns = self._all_num_idx = self._transform_dicts = None
-        if feature_sets is not None and numerical_idx is not None:
-            self._n_features = [len(feature_set) for feature_set in feature_sets]
-            self._categorical_columns = [
-                (i, value) for i, value in enumerate(self._n_features)
-                if not numerical_idx[i] and numerical_idx[i] is not None
-            ][:-1]
+        self.data_loaded = False
+        self.is_numeric_label = None
+        self.whether_redundant = None
+        self.numerical_idx = kwargs.pop("numerical_idx", None)
+        self.feature_sets = kwargs.pop("feature_sets", None)
+        self.sparsity = kwargs.pop("sparsity", None)
+        self.class_prior = kwargs.pop("class_prior", None)
+        self.dropout_keep_prob = kwargs.pop("p_keep", 0.5)
+        self.n_features = self.categorical_columns = self.all_num_idx = self.transform_dicts = None
+        if self.feature_sets is not None and self.numerical_idx is not None:
+            self.n_features = [len(feature_set) for feature_set in self.feature_sets]
+            self._gen_categorical_columns()
+
+        self.init_all_settings()
 
         file_type = kwargs.pop("file_type", "txt")
         shuffle = kwargs.pop("shuffle", True)
         restore = kwargs.pop("restore", True)
         test_rate = kwargs.pop("test_rate", 0.1)
-        args = (numerical_idx, file_type, shuffle, restore, test_rate)
+        args = (self.numerical_idx, file_type, shuffle, restore, test_rate)
         if x is None or y is None:
-            x, y, x_cv, y_cv = self._load_data(None, *args)
+            x, y, x_test, y_test = self._load_data(None, *args)
         else:
             data = np.hstack([x, y.reshape([-1, 1])])
-            if x_cv is not None and y_cv is not None:
-                data = (data, np.hstack([x_cv, y_cv.reshape([-1, 1])]))
-            x, y, x_cv, y_cv = self._load_data(data, *args)
+            if x_test is not None and y_test is not None:
+                data = (data, np.hstack([x_test, y_test.reshape([-1, 1])]))
+            x, y, x_test, y_test = self._load_data(data, *args)
+        if n_iter < 0:
+            n_iter = len(x) // batch_size + 1
+
+        self._sample_weights = kwargs.pop("sample_weights", None)
+        if self._sample_weights is not None and Toolbox.is_number(str(self._sample_weights)):
+            self._sample_weights = np.full(len(x), float(self._sample_weights))
+        self._handle_unbalance(y)
+        self._handle_sparsity()
 
         self._kwargs = kwargs
-        self._kwargs["numerical_idx"] = self._numerical_idx
-        self._kwargs["categorical_columns"] = self._categorical_columns
+        self._kwargs["p_keep"] = self.dropout_keep_prob
+        self._kwargs["numerical_idx"] = self.numerical_idx
+        self._kwargs["categorical_columns"] = self.categorical_columns
+        self._kwargs["sample_weights"] = self._sample_weights
 
         super(Auto, self).__init__(
-            x, y, x_cv, y_cv, name, loss, metric,
+            x, y, x_test, y_test, name, loss, metric,
             n_epoch, max_epoch, n_iter, batch_size, optimizer, lr, **kwargs
         )
 
     @property
     def label2num_dict(self):
-        return None if not self._transform_dicts[-1] else self._transform_dicts[-1]
+        return None if not self.transform_dicts[-1] else self.transform_dicts[-1]
 
     @property
     def num2label_dict(self):
@@ -96,15 +104,19 @@ class Auto(Advanced):
         return [elem for elem, redundant in zip(lst, whether_redundant) if not redundant]
 
     @staticmethod
-    def get_np_arrays(*arrs):
-        return [None if arr is None else np.asarray(arr, np.float32) for arr in arrs]
+    def get_np_arrays(*arrays):
+        return [None if arr is None else np.asarray(arr, np.float32) for arr in arrays]
 
     @staticmethod
-    def get_lists(*arrs):
-        return [
+    def get_lists(*arrays):
+        return tuple(
             arr if isinstance(arr, list) else np.asarray(arr, np.float32).tolist()
-            for arr in arrs
-        ]
+            for arr in arrays
+        )
+
+    def init_all_settings(self):
+        self.init_pre_process_settings()
+        self.init_nan_handler_settings()
 
     def init_pre_process_settings(self):
         self.pre_process_method = self._pre_process_settings.get("pre_process_method", "normalize")
@@ -119,18 +131,45 @@ class Auto(Advanced):
         self.nan_handler_method = self._nan_handler_settings.get("nan_handler_method", "median")
         self.reuse_nan_handler_values = self._nan_handler_settings.get("reuse_nan_handler_values", True)
 
-    def _transform_data(self, data, include_label=False, refresh_redundant_info=False, refresh_info=False):
+    def _handle_unbalance(self, y):
+        class_ratio = self.class_prior.min() / self.class_prior.max()
+        if class_ratio < 0.1:
+            warn_msg = "Sample weights will be used since class_ratio < 0.1 ({:8.6f})".format(class_ratio)
+            print(warn_msg)
+            if self._sample_weights is None:
+                print(
+                    "Sample weights are not provided, they'll be generated automatically"
+                )
+                self._sample_weights = np.ones(len(y)) / self.class_prior[y.astype(np.int)]
+                self._sample_weights /= self._sample_weights.sum()
+                self._sample_weights *= len(y)
+
+    def _handle_sparsity(self):
+        if self.sparsity >= 0.75:
+            warn_msg = "Dropout will be disabled since data sparsity >= 0.75 ({:8.6f})".format(self.sparsity)
+            print(warn_msg)
+            self.dropout_keep_prob = 1.
+
+    def _gen_categorical_columns(self):
+        self.categorical_columns = [
+            (i, value) for i, value in enumerate(self.n_features)
+            if not self.numerical_idx[i] and self.numerical_idx[i] is not None
+        ]
+        if not self.numerical_idx[-1]:
+            self.categorical_columns.pop()
+
+    def _transform_data(self, data, include_label=False, refresh_redundant_info=False):
         print("Transforming data")
-        label_dict = self._transform_dicts[-1]
-        if refresh_redundant_info:
-            self._whether_redundant = np.array([
+        label_dict = self.transform_dicts[-1]
+        if refresh_redundant_info or self.whether_redundant is None:
+            self.whether_redundant = np.array([
                 True if local_dict is None else False
-                for i, local_dict in enumerate(self._transform_dicts)
+                for i, local_dict in enumerate(self.transform_dicts)
             ])
         targets = [
             (i, local_dict) for i, (idx, local_dict) in enumerate(
-                zip(self._numerical_idx, self._transform_dicts)
-            ) if not idx and local_dict and not self._whether_redundant[i]
+                zip(self.numerical_idx, self.transform_dicts)
+            ) if not idx and local_dict and not self.whether_redundant[i]
         ][:-1]
         # Transform
         for line in data:
@@ -142,17 +181,17 @@ class Auto(Advanced):
                     line[i] = local_dict["nan"]
                 else:
                     line[i] = local_dict[elem]
-            if include_label and not self._is_numeric_label:
+            if include_label and not self.is_numeric_label:
                 line[-1] = label_dict[line[-1]]
         data = np.array(data, dtype=np.float32)
         # Handle nan
         if self._nan_handler is None:
-            self._nan_handler = NanHandler(self._numerical_idx, self.nan_handler_method)
+            self._nan_handler = NanHandler(self.numerical_idx, self.nan_handler_method)
         data = self._nan_handler.handle(data)
         # Handle redundant
-        n_redundant = np.sum(self._whether_redundant)
+        n_redundant = np.sum(self.whether_redundant)
         if n_redundant > 0:
-            whether_redundant = self._whether_redundant
+            whether_redundant = self.whether_redundant
             if not include_label:
                 whether_redundant = whether_redundant[:-1]
             if refresh_redundant_info:
@@ -162,49 +201,23 @@ class Auto(Advanced):
                     ", {} will be removed".format("it" if n_redundant == 1 else "they")
                 )
                 print(warn_msg)
-                self._numerical_idx = self.remove_redundant(whether_redundant, self._numerical_idx)
-                self._n_features = self.remove_redundant(whether_redundant, self._n_features)
+                self.numerical_idx = self.remove_redundant(whether_redundant, self.numerical_idx)
+                self.n_features = self.remove_redundant(whether_redundant, self.n_features)
             data = data[..., ~whether_redundant]
-        # Get sparsity and class_prior
-        if refresh_info:
-            x, y = data[..., :-1], data[..., -1]
-            self._sparsity = ((x == 0).sum() + np.isnan(x).sum()) / np.prod(x.shape)
-            _, class_counts = np.unique(y, return_counts=True)
-            self._class_prior = class_counts / class_counts.sum()
         # Pre-process data
         if self._pre_processor is not None:
             if not include_label:
-                data = self._pre_processor.process(data, self._numerical_idx[:-1])
+                data = self._pre_processor.process(data, self.numerical_idx[:-1])
             else:
-                data[..., :-1] = self._pre_processor.process(data[..., :-1], self._numerical_idx[:-1])
+                data[..., :-1] = self._pre_processor.process(data[..., :-1], self.numerical_idx[:-1])
         return data
 
-    def _prepare_xy(self, x, y, prefix, dtype=None):
-        if x is None or y is None:
-            if dtype is None:
-                dtype = "test" if self._test_data is not None else "train"
-            transform_y = False
-        else:
-            dtype = None
-            transform_y = True
-            x = self._transform_data(x)
-        if dtype is not None:
-            data = self._train_data if dtype == "train" else self._test_data
-            x, y = data[..., :-1], data[..., -1]
-        dic = self.num2label_dict
-        if transform_y and dic is not None:
-            if not self._is_numeric_label:
-                y = [dic[yy] for yy in y]
-            else:
-                y = [dic[int(float(yy))] for yy in y]
-        return np.asarray(x, np.float32), np.asarray(y, np.int)
-
     def _get_label_dict(self):
-        labels = self._feature_sets[-1]
+        labels = self.feature_sets[-1]
         if not all(Toolbox.is_number(str(label)) for label in labels):
-            self._is_numeric_label = False
+            self.is_numeric_label = False
             return {key: i for i, key in enumerate(sorted(labels))}
-        self._is_numeric_label = True
+        self.is_numeric_label = True
         label_set = set([int(float(label)) for label in labels])
         if -1 not in label_set:
             return {}
@@ -213,34 +226,36 @@ class Auto(Advanced):
         return {-1: 0, int(float(label_set.pop())): 1}
 
     def _get_transform_dicts(self):
-        self._transform_dicts = [
+        self.transform_dicts = [
             None if is_numerical is None else
             {key: i for i, key in enumerate(sorted(feature_set))}
-            if not is_numerical and not all_num else {}
-            for is_numerical, feature_set, all_num in zip(
-                self._numerical_idx[:-1], self._feature_sets[:-1], self._all_num_idx[:-1]
+            if not is_numerical and (not all_num or not np.allclose(
+                np.sort(np.array(list(feature_set), np.float32).astype(np.int32)),
+                np.arange(0, len(feature_set))
+            )) else {} for is_numerical, feature_set, all_num in zip(
+                self.numerical_idx[:-1], self.feature_sets[:-1], self.all_num_idx[:-1]
             )
         ]
         if self.n_class == 1:
-            self._transform_dicts.append({})
+            self.transform_dicts.append({})
         else:
-            self._transform_dicts.append(self._get_label_dict())
+            self.transform_dicts.append(self._get_label_dict())
 
     def _load_data(self, data=None, numerical_idx=None, file_type="txt",
-                  shuffle=True, restore=True, test_rate=0.1):
+                   shuffle=True, restore=True, test_rate=0.1):
         data_folder = os.path.join("_Data", "_Cache", self._name)
         data_info_file = os.path.join("_DataInfo", "{}.info".format(self._name))
         train_data_file = os.path.join(data_folder, "train.npy")
         test_data_file = os.path.join(data_folder, "test.npy")
 
         use_cached_data = False
-        if data is None:
-            if os.path.isfile(train_data_file):
-                print("Restoring data")
-                use_cached_data = True
-                train_data = np.load(train_data_file)
-                test_data = np.load(test_data_file) if os.path.isfile(test_data_file) else None
-                data = train_data if test_data is None else (train_data, test_data)
+        train_data = test_data = None
+        if data is None and restore and os.path.isfile(train_data_file):
+            print("Restoring data")
+            use_cached_data = True
+            train_data = np.load(train_data_file)
+            test_data = np.load(test_data_file) if os.path.isfile(test_data_file) else None
+            data = train_data if test_data is None else (train_data, test_data)
 
         if not use_cached_data:
             if file_type == "txt":
@@ -262,7 +277,7 @@ class Auto(Advanced):
                     data = (train_data, test_data)
             else:
                 if not isinstance(data, tuple):
-                    data = np.asarray(data, dtype=np.float32).tolist()
+                    data = np.asarray(data, dtype=np.float32).tolist()  # type: list
                 else:
                     data = self.get_lists(*data)
             if isinstance(data, tuple):
@@ -274,47 +289,44 @@ class Auto(Advanced):
                 if shuffle:
                     random.shuffle(data)
                 n_train = int(len(data) * (1 - test_rate)) if test_rate > 0 else -1
+        else:
+            n_train = None
 
         if not os.path.exists("_DataInfo"):
             os.makedirs("_DataInfo")
         if not os.path.isfile(data_info_file) or not restore:
             print("Generating data info")
             if numerical_idx is not None:
-                self._numerical_idx = numerical_idx
-            elif self._numerical_idx is not None:
-                numerical_idx = self._numerical_idx
-            if not self._feature_sets or not self._n_features or not self._all_num_idx:
-                self._feature_sets, self._n_features, self._all_num_idx, self._numerical_idx = (
+                self.numerical_idx = numerical_idx
+            elif self.numerical_idx is not None:
+                numerical_idx = self.numerical_idx
+            if not self.feature_sets or not self.n_features or not self.all_num_idx:
+                self.feature_sets, self.n_features, self.all_num_idx, self.numerical_idx = (
                     Toolbox.get_feature_info(data, numerical_idx)
                 )
-            self.n_class = self._n_features[-1]
+            self.n_class = self.n_features[-1]
             self._get_transform_dicts()
             with open(data_info_file, "wb") as file:
                 pickle.dump([
-                    self._n_features, self._numerical_idx, self._transform_dicts, self._is_numeric_label
+                    self.n_features, self.numerical_idx, self.transform_dicts, self.is_numeric_label
                 ], file)
         else:
             print("Restoring data info")
             with open(data_info_file, "rb") as file:
                 data_info = pickle.load(file)
-                self._n_features, self._numerical_idx, self._transform_dicts, self._is_numeric_label = data_info
-            self.n_class = self._n_features[-1]
+                self.n_features, self.numerical_idx, self.transform_dicts, self.is_numeric_label = data_info
+            self.n_class = self.n_features[-1]
 
         if not use_cached_data:
             if n_train > 0:
                 train_data, test_data = data[:n_train], data[n_train:]
             else:
                 train_data, test_data = data, None
-            train_data = self._transform_data(train_data, True, True, True)
+            train_data = self._transform_data(train_data, True, True)
             if test_data is not None:
                 test_data = self._transform_data(test_data, True)
 
-        self._categorical_columns = [
-            (i, value) for i, value in enumerate(self._n_features)
-            if not self._numerical_idx[i] and self._numerical_idx[i] is not None
-        ]
-        if not self._numerical_idx[-1]:
-            self._categorical_columns.pop()
+        self._gen_categorical_columns()
 
         if not use_cached_data:
             print("Caching data...")
@@ -323,13 +335,16 @@ class Auto(Advanced):
             np.save(train_data_file, train_data)
             if test_data is not None:
                 np.save(test_data_file, test_data)
-        self._data_loaded = True
+        self.data_loaded = True
 
         x, y = train_data[..., :-1], train_data[..., -1]
         if test_data is not None:
             x_test, y_test = test_data[..., :-1], test_data[..., -1]
         else:
             x_test = y_test = None
+        self.sparsity = ((x == 0).sum() + np.isnan(x).sum()) / np.prod(x.shape)
+        _, class_counts = np.unique(y, return_counts=True)
+        self.class_prior = class_counts / class_counts.sum()
 
         return x, y, x_test, y_test
 
@@ -344,12 +359,11 @@ class Auto(Advanced):
         ]
 
     def predict(self, x):
-        x = self._transform_data(x)
-        output = self._calculate(x, is_training=False)
-        if self.n_class == 1:
-            return output.ravel()
-        return output
+        return self._predict(self._transform_data(x))
 
-
-if __name__ == "__main__":
-    auto = Auto(name="Zhongan").fit()
+    def predict_target_prob(self, x, target):
+        prob = self.predict(x)
+        label2num_dict = self.label2num_dict
+        if label2num_dict is not None:
+            target = label2num_dict[target]
+        return prob[..., target]
