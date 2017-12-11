b2cc670b0d2beace5661fa6bb3327e492d15d07e
carefree0910 <syameimaru.saki@gmail.com>
2017-12-12 00:09:48 +0800

    Upload _Dist/NeuralNetworks/_Tests/Madelon

---
+++ _Dist/NeuralNetworks/_Tests/Madelon/TestDT2NN.py
@@ -0,0 +1,22 @@
+import os
+import sys
+root_path = os.path.abspath("../../../../")
+if root_path not in sys.path:
+    sys.path.append(root_path)
+
+import numpy as np
+
+from _Dist.NeuralNetworks.NNUtil import Toolbox
+from _Dist.NeuralNetworks.c_BasicNN.NN import Basic
+from _Dist.NeuralNetworks.d_Traditional2NN.Toolbox import DT2NN
+
+with open(os.path.join(root_path, "_Data", "madelon.txt"), "r") as file:
+    data = np.array(Toolbox.get_data(file), np.float32)
+train_set, test_set = data[:2000], data[2000:]
+x, y = train_set[..., :-1], train_set[..., -1]
+x_test, y_test = test_set[..., :-1], test_set[..., -1]
+
+dt2dnn = DT2NN(
+    model_param_settings={"activations": ["sign", "softmax"]}
+).fit(x, y, x_test, y_test)
+basic = Basic().fit(x, y, x_test, y_test)
