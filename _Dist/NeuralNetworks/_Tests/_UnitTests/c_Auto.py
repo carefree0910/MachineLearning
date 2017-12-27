07254e367b21969127dd872615935d0c8bffb66b
carefree0910 <syameimaru.saki@gmail.com>
2017-12-27 17:03:23 +0800

    Update _Tests/_UnitTests

--- _Dist/NeuralNetworks/_Tests/_UnitTests/c_Auto.py
+++ _Dist/NeuralNetworks/_Tests/_UnitTests/c_Auto.py
@@ -10,8 +10,8 @@ import unittest
 import numpy as np
 
 from Util.Util import DataUtil
-from _Dist.NeuralNetworks.f_AutoNN.NN import AutoAdvanced
 from _Dist.NeuralNetworks.b_TraditionalML.SVM import AutoLinearSVM
+from _Dist.NeuralNetworks.f_AutoNN.NN import AutoBasic, AutoAdvanced
 from _Dist.NeuralNetworks._Tests._UnitTests.UnitTestUtil import clear_cache
 
 
@@ -20,6 +20,7 @@ base_params = {
     "model_param_settings": {"n_epoch": 1, "max_epoch": 2}
 }
 nn = AutoAdvanced(**copy.deepcopy(base_params))
+basic_nn = AutoBasic(**copy.deepcopy(base_params))
 linear_svm = AutoLinearSVM(**copy.deepcopy(base_params))
 train_set, test_set = DataUtil.gen_noisy_linear(1000, 2, 2, one_hot=False)
 
@@ -30,14 +31,16 @@ auto_mushroom_params["data_info"]["data_folder"] = "../_Data"
 mushroom_labels = {"p", "e"}
 mushroom_file = "../_Data/mushroom"
 auto_mushroom_nn = AutoAdvanced(**copy.deepcopy(auto_mushroom_params))
+auto_mushroom_basic_nn = AutoBasic(**copy.deepcopy(auto_mushroom_params))
 auto_mushroom_linear_svm = AutoLinearSVM(**copy.deepcopy(auto_mushroom_params))
 
 auto_adult_params = copy.deepcopy(base_params)
-auto_adult_params["name"] = "adult"
+auto_adult_params["name"] = "Adult"
 auto_adult_params["data_info"]["file_type"] = "csv"
 auto_adult_params["data_info"]["data_folder"] = "../_Data"
 adult_file = "../_Data/Adult/test"
 auto_adult_nn = AutoAdvanced(**copy.deepcopy(auto_adult_params))
+auto_adult_basic_nn = AutoBasic(**copy.deepcopy(auto_adult_params))
 auto_adult_linear_svm = AutoLinearSVM(**copy.deepcopy(auto_adult_params))
 
 auto_lmgpip_params = copy.deepcopy(base_params)
@@ -46,10 +49,12 @@ auto_lmgpip_params["data_info"]["file_type"] = "txt"
 auto_lmgpip_params["data_info"]["data_folder"] = "../_Data"
 lmgpip_test_file = "../_Data/lmgpip_test"
 auto_lmgpip_nn = AutoAdvanced(**auto_lmgpip_params)
+auto_lmgpip_basic_nn = AutoBasic(**auto_lmgpip_params)
 
 auto_lmgpip_regressor_params = copy.deepcopy(auto_lmgpip_params)
 auto_lmgpip_regressor_params["data_info"]["is_regression"] = True
 auto_lmgpip_regressor = AutoAdvanced(**auto_lmgpip_regressor_params)
+auto_lmgpip_basic_regressor = AutoBasic(**auto_lmgpip_regressor_params)
 
 
 class TestAutoNN(unittest.TestCase):
@@ -58,32 +63,48 @@ class TestAutoNN(unittest.TestCase):
             nn.fit(*train_set, *test_set, verbose=0), AutoAdvanced,
             msg="Train failed"
         )
+        self.assertIsInstance(
+            basic_nn.fit(*train_set, *test_set, verbose=0), AutoBasic,
+            msg="Train failed"
+        )
 
     def test_01_predict(self):
         self.assertIs(nn.predict(train_set[0]).dtype, np.dtype("float32"), "Predict failed")
+        self.assertIs(basic_nn.predict(train_set[0]).dtype, np.dtype("float32"), "Predict failed")
         self.assertIs(nn.predict_classes(test_set[0]).dtype, np.dtype("int32"), "Predict classes failed")
+        self.assertIs(basic_nn.predict_classes(test_set[0]).dtype, np.dtype("int32"), "Predict classes failed")
 
     def test_02_evaluate(self):
         self.assertEqual(len(nn.evaluate(*train_set, *test_set)), 3, "Evaluation failed")
 
     def test_03_save(self):
         self.assertIsInstance(nn.save(), AutoAdvanced, msg="Save failed")
+        self.assertIsInstance(basic_nn.save(), AutoBasic, msg="Save failed")
 
     def test_04_load(self):
-        global nn
+        global nn, basic_nn
         nn = AutoAdvanced(**base_params).load()
+        basic_nn = AutoBasic(**base_params).load()
         self.assertIsInstance(nn, AutoAdvanced, "Load failed")
+        self.assertIsInstance(basic_nn, AutoBasic, "Load failed")
 
     def test_05_re_predict(self):
         self.assertIs(nn.predict(train_set[0]).dtype, np.dtype("float32"), "Re-Predict failed")
+        self.assertIs(basic_nn.predict(train_set[0]).dtype, np.dtype("float32"), "Re-Predict failed")
         self.assertIs(nn.predict_classes(test_set[0]).dtype, np.dtype("int32"), "Re-Predict classes failed")
+        self.assertIs(basic_nn.predict_classes(test_set[0]).dtype, np.dtype("int32"), "Re-Predict classes failed")
 
     def test_06_re_evaluate(self):
         self.assertEqual(len(nn.evaluate(*train_set, *test_set)), 3, "Re-Evaluation failed")
+        self.assertEqual(len(basic_nn.evaluate(*train_set, *test_set)), 3, "Re-Evaluation failed")
 
     def test_07_re_train(self):
         self.assertIsInstance(
             nn.fit(*train_set, *test_set, verbose=0), AutoAdvanced,
+            msg="Re-Train failed"
+        )
+        self.assertIsInstance(
+            basic_nn.fit(*train_set, *test_set, verbose=0), AutoBasic,
             msg="Re-Train failed"
         )
 
@@ -92,52 +113,65 @@ class TestAutoNN(unittest.TestCase):
             auto_mushroom_nn.fit(verbose=0), AutoAdvanced,
             msg="Train failed"
         )
+        self.assertIsInstance(
+            auto_mushroom_basic_nn.fit(verbose=0), AutoBasic,
+            msg="Train failed"
+        )
 
     def test_09_predict_from_txt(self):
-        self.assertIs(
-            auto_mushroom_nn.predict_from_file(
+        for local_nn in (auto_mushroom_nn, auto_mushroom_basic_nn):
+            self.assertIs(
+                local_nn.predict_from_file(
+                    mushroom_file, "txt", include_label=True
+                ).dtype, np.dtype("float32"), msg="Predict failed"
+            )
+            self.assertIs(
+                local_nn.predict_classes_from_file(
+                    mushroom_file, "txt", include_label=True
+                ).dtype, np.dtype("int32"), msg="Predict classes failed"
+            )
+            predict_labels = np.unique(local_nn.predict_labels_from_file(
                 mushroom_file, "txt", include_label=True
-            ).dtype, np.dtype("float32"), msg="Predict failed"
-        )
-        self.assertIs(
-            auto_mushroom_nn.predict_classes_from_file(
-                mushroom_file, "txt", include_label=True
-            ).dtype, np.dtype("int32"), msg="Predict classes failed"
-        )
-        predict_labels = np.unique(auto_mushroom_nn.predict_labels_from_file(
-            mushroom_file, "txt", include_label=True
-        ))
-        for pred_label in predict_labels:
-            self.assertIn(pred_label, mushroom_labels, msg="Predict labels failed")
+            ))
+            for pred_label in predict_labels:
+                self.assertIn(pred_label, mushroom_labels, msg="Predict labels failed")
 
     def test_10_save(self):
         self.assertIsInstance(auto_mushroom_nn.save(), AutoAdvanced, msg="Save failed")
+        self.assertIsInstance(auto_mushroom_basic_nn.save(), AutoBasic, msg="Save failed")
 
     def test_11_load(self):
-        global auto_mushroom_nn
+        global auto_mushroom_nn, auto_mushroom_basic_nn
         auto_mushroom_nn = AutoAdvanced(**auto_mushroom_params).load()
+        auto_mushroom_basic_nn = AutoBasic(**auto_mushroom_params).load()
         self.assertIsInstance(auto_mushroom_nn, AutoAdvanced, "Load failed")
+        self.assertIsInstance(auto_mushroom_basic_nn, AutoBasic, "Load failed")
 
     def test_12_re_predict_from_txt(self):
-        self.assertIs(
-            auto_mushroom_nn.predict_from_file(
+        for local_nn in (auto_mushroom_nn, auto_mushroom_basic_nn):
+            self.assertIs(
+                local_nn.predict_from_file(
+                    mushroom_file, "txt", include_label=True
+                ).dtype, np.dtype("float32"), msg="Re-Predict failed"
+            )
+            self.assertIs(
+                local_nn.predict_classes_from_file(
+                    mushroom_file, "txt", include_label=True
+                ).dtype, np.dtype("int32"), msg="Re-Predict classes failed"
+            )
+            predict_labels = np.unique(local_nn.predict_labels_from_file(
                 mushroom_file, "txt", include_label=True
-            ).dtype, np.dtype("float32"), msg="Re-Predict failed"
-        )
-        self.assertIs(
-            auto_mushroom_nn.predict_classes_from_file(
-                mushroom_file, "txt", include_label=True
-            ).dtype, np.dtype("int32"), msg="Re-Predict classes failed"
-        )
-        predict_labels = np.unique(auto_mushroom_nn.predict_labels_from_file(
-            mushroom_file, "txt", include_label=True
-        ))
-        for pred_label in predict_labels:
-            self.assertIn(pred_label, mushroom_labels, msg="Re-Predict labels failed")
+            ))
+            for pred_label in predict_labels:
+                self.assertIn(pred_label, mushroom_labels, msg="Re-Predict labels failed")
 
     def test_13_re_train_from_txt(self):
         self.assertIsInstance(
             auto_mushroom_nn.fit(verbose=0), AutoAdvanced,
+            msg="Re-Train failed"
+        )
+        self.assertIsInstance(
+            auto_mushroom_basic_nn.fit(verbose=0), AutoBasic,
             msg="Re-Train failed"
         )
 
@@ -146,42 +180,55 @@ class TestAutoNN(unittest.TestCase):
             auto_adult_nn.fit(verbose=0), AutoAdvanced,
             msg="Train failed"
         )
+        self.assertIsInstance(
+            auto_adult_basic_nn.fit(verbose=0), AutoBasic,
+            msg="Train failed"
+        )
 
     def test_15_predict_from_csv(self):
-        self.assertIs(
-            auto_adult_nn.predict_from_file(
-                adult_file, "csv", include_label=True
-            ).dtype, np.dtype("float32"), msg="Predict failed"
-        )
-        self.assertIs(
-            auto_adult_nn.predict_classes_from_file(
-                adult_file, "csv", include_label=True
-            ).dtype, np.dtype("int32"), msg="Predict classes failed"
-        )
+        for local_nn in (auto_adult_nn, auto_adult_basic_nn):
+            self.assertIs(
+                local_nn.predict_from_file(
+                    adult_file, "csv", include_label=True
+                ).dtype, np.dtype("float32"), msg="Predict failed"
+            )
+            self.assertIs(
+                local_nn.predict_classes_from_file(
+                    adult_file, "csv", include_label=True
+                ).dtype, np.dtype("int32"), msg="Predict classes failed"
+            )
 
     def test_16_save(self):
         self.assertIsInstance(auto_adult_nn.save(), AutoAdvanced, msg="Save failed")
+        self.assertIsInstance(auto_adult_basic_nn.save(), AutoBasic, msg="Save failed")
 
     def test_17_load(self):
-        global auto_adult_nn
+        global auto_adult_nn, auto_adult_basic_nn
         auto_adult_nn = AutoAdvanced(**auto_adult_params).load()
+        auto_adult_basic_nn = AutoBasic(**auto_adult_params).load()
         self.assertIsInstance(auto_adult_nn, AutoAdvanced, "Load failed")
+        self.assertIsInstance(auto_adult_basic_nn, AutoBasic, "Load failed")
 
     def test_18_re_predict_from_csv(self):
-        self.assertIs(
-            auto_adult_nn.predict_from_file(
-                adult_file, "csv", include_label=True
-            ).dtype, np.dtype("float32"), msg="Re-Predict failed"
-        )
-        self.assertIs(
-            auto_adult_nn.predict_classes_from_file(
-                adult_file, "csv", include_label=True
-            ).dtype, np.dtype("int32"), msg="Re-Predict classes failed"
-        )
+        for local_nn in (auto_adult_nn, auto_adult_basic_nn):
+            self.assertIs(
+                local_nn.predict_from_file(
+                    adult_file, "csv", include_label=True
+                ).dtype, np.dtype("float32"), msg="Re-Predict failed"
+            )
+            self.assertIs(
+                local_nn.predict_classes_from_file(
+                    adult_file, "csv", include_label=True
+                ).dtype, np.dtype("int32"), msg="Re-Predict classes failed"
+            )
 
     def test_19_re_train_from_csv(self):
         self.assertIsInstance(
             auto_adult_nn.fit(verbose=0), AutoAdvanced,
+            msg="Re-Train failed"
+        )
+        self.assertIsInstance(
+            auto_adult_basic_nn.fit(verbose=0), AutoBasic,
             msg="Re-Train failed"
         )
 
@@ -190,42 +237,55 @@ class TestAutoNN(unittest.TestCase):
             auto_lmgpip_nn.fit(verbose=0), AutoAdvanced,
             msg="Train failed"
         )
+        self.assertIsInstance(
+            auto_lmgpip_basic_nn.fit(verbose=0), AutoBasic,
+            msg="Train failed"
+        )
 
     def test_21_predict_from_mixed_features(self):
-        self.assertIs(
-            auto_lmgpip_nn.predict_from_file(
-                lmgpip_test_file, "txt", include_label=False
-            ).dtype, np.dtype("float32"), msg="Predict failed"
-        )
-        self.assertIs(
-            auto_lmgpip_nn.predict_classes_from_file(
-                lmgpip_test_file, "txt", include_label=False
-            ).dtype, np.dtype("int32"), msg="Predict classes failed"
-        )
+        for local_nn in (auto_lmgpip_nn, auto_lmgpip_basic_nn):
+            self.assertIs(
+                local_nn.predict_from_file(
+                    lmgpip_test_file, "txt", include_label=False
+                ).dtype, np.dtype("float32"), msg="Predict failed"
+            )
+            self.assertIs(
+                local_nn.predict_classes_from_file(
+                    lmgpip_test_file, "txt", include_label=False
+                ).dtype, np.dtype("int32"), msg="Predict classes failed"
+            )
 
     def test_22_save(self):
         self.assertIsInstance(auto_lmgpip_nn.save(), AutoAdvanced, msg="Save failed")
+        self.assertIsInstance(auto_lmgpip_basic_nn.save(), AutoBasic, msg="Save failed")
 
     def test_23_load(self):
-        global auto_lmgpip_nn
+        global auto_lmgpip_nn, auto_lmgpip_basic_nn
         auto_lmgpip_nn = AutoAdvanced(**auto_lmgpip_params).load()
+        auto_lmgpip_basic_nn = AutoBasic(**auto_lmgpip_params).load()
         self.assertIsInstance(auto_lmgpip_nn, AutoAdvanced, "Load failed")
+        self.assertIsInstance(auto_lmgpip_basic_nn, AutoBasic, "Load failed")
 
     def test_24_re_predict_from_mixed_features(self):
-        self.assertIs(
-            auto_lmgpip_nn.predict_from_file(
-                lmgpip_test_file, "txt", include_label=False
-            ).dtype, np.dtype("float32"), msg="Re-Predict failed"
-        )
-        self.assertIs(
-            auto_lmgpip_nn.predict_classes_from_file(
-                lmgpip_test_file, "txt", include_label=False
-            ).dtype, np.dtype("int32"), msg="Re-Predict classes failed"
-        )
+        for local_nn in (auto_lmgpip_nn, auto_lmgpip_basic_nn):
+            self.assertIs(
+                local_nn.predict_from_file(
+                    lmgpip_test_file, "txt", include_label=False
+                ).dtype, np.dtype("float32"), msg="Re-Predict failed"
+            )
+            self.assertIs(
+                local_nn.predict_classes_from_file(
+                    lmgpip_test_file, "txt", include_label=False
+                ).dtype, np.dtype("int32"), msg="Re-Predict classes failed"
+            )
 
     def test_25_re_train_from_mixed_features(self):
         self.assertIsInstance(
             auto_lmgpip_nn.fit(verbose=0), AutoAdvanced,
+            msg="Re-Train failed"
+        )
+        self.assertIsInstance(
+            auto_lmgpip_basic_nn.fit(verbose=0), AutoBasic,
             msg="Re-Train failed"
         )
 
@@ -236,40 +296,53 @@ class TestAutoNN(unittest.TestCase):
             auto_lmgpip_regressor.fit(verbose=0), AutoAdvanced,
             msg="Train failed"
         )
+        self.assertIsInstance(
+            auto_lmgpip_basic_regressor.fit(verbose=0), AutoBasic,
+            msg="Train failed"
+        )
 
     def test_27_predict_regressor_from_mixed_features(self):
-        self.assertIs(
-            auto_lmgpip_regressor.predict_from_file(
-                lmgpip_test_file, "txt", include_label=False
-            ).dtype, np.dtype("float32"), msg="Predict failed"
-        )
-        with self.assertRaises(ValueError):
-            auto_lmgpip_regressor.predict_classes_from_file(
-                lmgpip_test_file, "txt", include_label=False
+        for local_nn in (auto_lmgpip_regressor, auto_lmgpip_basic_regressor):
+            self.assertIs(
+                local_nn.predict_from_file(
+                    lmgpip_test_file, "txt", include_label=False
+                ).dtype, np.dtype("float32"), msg="Predict failed"
             )
+            with self.assertRaises(ValueError):
+                local_nn.predict_classes_from_file(
+                    lmgpip_test_file, "txt", include_label=False
+                )
 
     def test_28_save(self):
         self.assertIsInstance(auto_lmgpip_regressor.save(), AutoAdvanced, msg="Save failed")
+        self.assertIsInstance(auto_lmgpip_basic_regressor.save(), AutoBasic, msg="Save failed")
 
     def test_29_load(self):
-        global auto_lmgpip_regressor
+        global auto_lmgpip_regressor, auto_lmgpip_basic_regressor
         auto_lmgpip_regressor = AutoAdvanced(**auto_lmgpip_regressor_params).load()
+        auto_lmgpip_basic_regressor = AutoBasic(**auto_lmgpip_regressor_params).load()
         self.assertIsInstance(auto_lmgpip_regressor, AutoAdvanced, "Load failed")
+        self.assertIsInstance(auto_lmgpip_basic_regressor, AutoBasic, "Load failed")
 
     def test_30_re_predict_regressor_from_mixed_features(self):
-        self.assertIs(
-            auto_lmgpip_regressor.predict_from_file(
-                lmgpip_test_file, "txt", include_label=False
-            ).dtype, np.dtype("float32"), msg="Re-Predict failed"
-        )
-        with self.assertRaises(ValueError):
-            auto_lmgpip_regressor.predict_classes_from_file(
-                lmgpip_test_file, "txt", include_label=False
+        for local_nn in (auto_lmgpip_regressor, auto_lmgpip_basic_regressor):
+            self.assertIs(
+                local_nn.predict_from_file(
+                    lmgpip_test_file, "txt", include_label=False
+                ).dtype, np.dtype("float32"), msg="Re-Predict failed"
             )
+            with self.assertRaises(ValueError):
+                local_nn.predict_classes_from_file(
+                    lmgpip_test_file, "txt", include_label=False
+                )
 
     def test_31_re_train_regressor_from_mixed_features(self):
         self.assertIsInstance(
             auto_lmgpip_regressor.fit(verbose=0), AutoAdvanced,
+            msg="Re-Train failed"
+        )
+        self.assertIsInstance(
+            auto_lmgpip_basic_regressor.fit(verbose=0), AutoBasic,
             msg="Re-Train failed"
         )
 
--- _Dist/NeuralNetworks/_Tests/_UnitTests/d_Dist.py
+++ _Dist/NeuralNetworks/_Tests/_UnitTests/d_Dist.py
@@ -9,8 +9,8 @@ import unittest
 import numpy as np
 
 from Util.Util import DataUtil
-from _Dist.NeuralNetworks.g_DistNN.NN import DistAdvanced
 from _Dist.NeuralNetworks.b_TraditionalML.SVM import DistLinearSVM
+from _Dist.NeuralNetworks.g_DistNN.NN import DistBasic, DistAdvanced
 from _Dist.NeuralNetworks._Tests._UnitTests.UnitTestUtil import clear_cache
 
 
@@ -19,6 +19,7 @@ base_params = {
     "model_param_settings": {"n_epoch": 1, "max_epoch": 2}
 }
 nn = DistAdvanced(**copy.deepcopy(base_params))
+basic_nn = DistBasic(**copy.deepcopy(base_params))
 linear_svm = DistLinearSVM(**copy.deepcopy(base_params))
 train_set, test_set = DataUtil.gen_noisy_linear(1000, 2, 2, one_hot=False)
 (x, y), (x_test, y_test) = train_set, test_set
@@ -36,28 +37,45 @@ class TestDistNN(unittest.TestCase):
             nn.k_fold(3, (train_data, test_data), verbose=0), DistAdvanced,
             msg="k-fold failed"
         )
+        self.assertIsInstance(
+            basic_nn.k_random(3, (train_data, test_data), verbose=0), DistBasic,
+            msg="k-random failed"
+        )
+        self.assertIsInstance(
+            basic_nn.k_fold(3, (train_data, test_data), verbose=0), DistBasic,
+            msg="k-fold failed"
+        )
 
     def test_01_predict(self):
         self.assertIs(nn.predict(train_set[0]).dtype, np.dtype("float32"), "Predict failed")
         self.assertIs(nn.predict_classes(test_set[0]).dtype, np.dtype("int32"), "Predict classes failed")
+        self.assertIs(basic_nn.predict(train_set[0]).dtype, np.dtype("float32"), "Predict failed")
+        self.assertIs(basic_nn.predict_classes(test_set[0]).dtype, np.dtype("int32"), "Predict classes failed")
 
     def test_02_evaluate(self):
         self.assertEqual(len(nn.evaluate(*train_set, *test_set)), 3, "Evaluation failed")
+        self.assertEqual(len(basic_nn.evaluate(*train_set, *test_set)), 3, "Evaluation failed")
 
     def test_03_save(self):
         self.assertIsInstance(nn.save(), DistAdvanced, msg="Save failed")
+        self.assertIsInstance(basic_nn.save(), DistBasic, msg="Save failed")
 
     def test_04_load(self):
-        global nn
-        model = DistAdvanced(**base_params).load()
-        self.assertIsInstance(model, DistAdvanced, "Load failed")
+        global nn, basic_nn
+        nn = DistAdvanced(**base_params).load()
+        basic_nn = DistBasic(**base_params).load()
+        self.assertIsInstance(nn, DistAdvanced, "Load failed")
+        self.assertIsInstance(basic_nn, DistBasic, "Load failed")
 
     def test_05_re_predict(self):
         self.assertIs(nn.predict(train_set[0]).dtype, np.dtype("float32"), "Re-Predict failed")
         self.assertIs(nn.predict_classes(test_set[0]).dtype, np.dtype("int32"), "Re-Predict classes failed")
+        self.assertIs(basic_nn.predict(train_set[0]).dtype, np.dtype("float32"), "Re-Predict failed")
+        self.assertIs(basic_nn.predict_classes(test_set[0]).dtype, np.dtype("int32"), "Re-Predict classes failed")
 
     def test_06_re_evaluate(self):
         self.assertEqual(len(nn.evaluate(*train_set, *test_set)), 3, "Re-Evaluation failed")
+        self.assertEqual(len(basic_nn.evaluate(*train_set, *test_set)), 3, "Re-Evaluation failed")
 
     def test_07_param_search(self):
         params = [
@@ -66,6 +84,10 @@ class TestDistNN(unittest.TestCase):
         ]
         self.assertIsInstance(
             nn.param_search(params, data=(train_data, test_data), verbose=0), DistAdvanced,
+            msg="param_search failed"
+        )
+        self.assertIsInstance(
+            basic_nn.param_search(params, data=(train_data, test_data), verbose=0), DistBasic,
             msg="param_search failed"
         )
 
@@ -105,6 +127,18 @@ class TestDistNN(unittest.TestCase):
                 data=(train_data, test_data), verbose=0
             ), DistAdvanced, msg="dict_first_grid_search failed"
         )
+        self.assertIsInstance(
+            basic_nn.random_search(
+                4, list_first_grid_params, grid_order="list_first",
+                data=(train_data, test_data), verbose=0
+            ), DistBasic, msg="list_first_grid_search failed"
+        )
+        self.assertIsInstance(
+            basic_nn.random_search(
+                8, dict_first_grid_params, grid_order="dict_first",
+                data=(train_data, test_data), verbose=0
+            ), DistBasic, msg="dict_first_grid_search failed"
+        )
 
     def test_09_grid_search(self):
         list_first_grid_params = {
@@ -119,11 +153,11 @@ class TestDistNN(unittest.TestCase):
         }
         dict_first_grid_params = {
             "model_param_settings": {
-                "lr": [1e-2, 1e-3],
+                "lr": [1e-3, 1e-2],
                 "loss": ["mse", "cross_entropy"]
             },
             "model_structure_settings": {
-                "hidden_units": [[128, 128], [256, 256]]
+                "hidden_units": [[128, 256], [128, 256]]
             }
         }
         self.assertIsInstance(
@@ -138,11 +172,23 @@ class TestDistNN(unittest.TestCase):
                 data=(train_data, test_data), verbose=0
             ), DistAdvanced, msg="dict_first_grid_search failed"
         )
+        self.assertIsInstance(
+            basic_nn.grid_search(
+                list_first_grid_params, grid_order="list_first",
+                data=(train_data, test_data), verbose=0
+            ), DistBasic, msg="list_first_grid_search failed"
+        )
+        self.assertIsInstance(
+            basic_nn.grid_search(
+                dict_first_grid_params, grid_order="dict_first",
+                data=(train_data, test_data), verbose=0
+            ), DistBasic, msg="dict_first_grid_search failed"
+        )
 
     def test_10_range_search(self):
         range_grid_params = {
             "model_param_settings": {
-                "lr": ["float", 1e-1, 1e-3],
+                "lr": ["float", 1e-3, 1e-1, "log"],
                 "loss": ["choice", ["mse", "cross_entropy"]]
             },
             "model_structure_settings": {
@@ -151,9 +197,9 @@ class TestDistNN(unittest.TestCase):
                     [128, 256], [128, 256]
                 ],
                 "pruner_params": {
-                    "alpha": ["float", 1e-2, 1e-4],
-                    "beta": ["float", 0.3, 3],
-                    "gamma": ["float", 0.5, 2]
+                    "alpha": ["float", 1e-4, 1e-2, "log"],
+                    "beta": ["float", 0.3, 3, "log"],
+                    "gamma": ["float", 0.5, 2, "log"]
                 }
             },
             "pre_process_settings": {
@@ -170,6 +216,12 @@ class TestDistNN(unittest.TestCase):
                 8, range_grid_params,
                 data=(train_data, test_data), verbose=0
             ), DistAdvanced, msg="range_search failed"
+        )
+        self.assertIsInstance(
+            basic_nn.range_search(
+                8, range_grid_params,
+                data=(train_data, test_data), verbose=0
+            ), DistBasic, msg="range_search failed"
         )
 
     def test_99_clear_cache(self):
