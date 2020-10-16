0170718fa889e557ed4adae708cb3bf7c7c1138a
Arjun <72355507+AnonumusArjun@users.noreply.github.com>
2020-10-16 10:32:24 +0530

    Iris data with Naive byes & explain with iris example

--- _SKlearn/NaiveBayes.py
+++ _SKlearn/NaiveBayes.py
@@ -16,3 +16,26 @@ class SKMultinomialNB(nb.MultinomialNB, ClassifierBase, metaclass=SKCompatibleMe
 
 class SKGaussianNB(nb.GaussianNB, ClassifierBase, metaclass=SKCompatibleMeta):
     pass
+
+from sklearn.datasets import load_iris 
+iris = load_iris() 
+  
+# store the feature matrix (X) and response vector (y) 
+X = iris.data 
+y = iris.target 
+  
+# splitting X and y into training and testing sets 
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1) 
+  
+# training the model on training set 
+from sklearn.naive_bayes import GaussianNB 
+gnb = GaussianNB() 
+gnb.fit(X_train, y_train) 
+  
+# making predictions on the testing set 
+y_pred = gnb.predict(X_test) 
+  
+# comparing actual response values (y_test) with predicted response values (y_pred) 
+from sklearn import metrics 
+print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
